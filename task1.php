e521cb7fdd865db8c7bfed8860082b8132022230
MatejMedvescek755 <48921892+MatejMedvescek755@users.noreply.github.com>
2026-04-19 10:13:41 +0200

    finish task1 and is subtasks

--- task1.php
+++ task1.php
@@ -1,3 +1,72 @@
 <?php 
-    phpinfo();
+    //subtask1
+    //phpinfo();
+
+    //subtask 2
+    // $krepko = "Boj med Rossijem in Marquezom je razdelil športno javnost in tudi poznavalce. Niko Mihelič: 'Rossi je izpadel kot pobalin'. Stane Kos oporeka: 'Ni jasno videti, da je Rossi Marqueza brcnil.'";
+    // $besedilo = "Redko se zgodi, da dirka motociklistov tako razvname strasti, toda nedeljsko dogajanje v Sepangu so včeraj komentirali tudi v frizerskih salonih. Na VN Malezije je namreč v razredu MotoGP Valentino Rossi na sporen način s steze zrinil Marca Marqueza. Ali se je Doktor, kot pravijo Rossiju, res požvižgal na vsa pravila športne etike in pri tem celo brcnil tekmeca ali pa v želji po naslovu prvaka njegovo dejanje lahko razumemo, in kaj za skupni seštevek svetovnega prvenstva pomeni, da bo Rossi (zaradi kazni) na zadnji dirki (8. novembra v Valencii) štartal z najslabšega položaja, smo se pogovarjali z dvema poznavalcema motošporta z RTV Slovenija Stanetom Kosom in Nikom Miheličem, ki imata povsem nasprotujoča si mnenja.";
+    // echo "<h1>Je Doktor Rossi res izbral pravi recept? </h1>";
+    // echo "<p>Obmebi po razburljivi dirki maletzije</p>";
+    // echo "<b>".$krepko."</b> <br />";
+    // echo $besedilo;
+
+    //subtask3 
+    // $y = 0;
+    // $krog = "krog";
+    // $krozni_lok = "lok";
+    // $moja_spremenljivka = "vrednost";
+    // $stevilo = 10;
+    // $stevec = 0;
+
+    // $kvadratna_enacba = "";
+    // $stevilo_avtomobilov = 5;
+    // $aktiven_gumb = true;
+    // $prestej_zrele_cesnje = 0;
+
+    // $hrušk12 = 12;
+    // $vaja_20 = "vaja";
+    // $info_domena_com = "email";
+    // $znak_dolar = "$";
+    // $a_b_c_d_e = "seznam";
+    // $alfa = "a";
+    // $euro = "€";
+
+    // $novi_objekt = "new";
+    // $prekinitev = "break";
+    // $konstanta_var = "const";
+
+    // $pot_mapa = "c:\\mapa";
+    // $url_naslov = "http://www.ime.si";
+    // $gori_na_gori_gori = "besedilo";
+    //subtask4
+    $x = 25;
+    $y = -17;
+    $krog = 236589;
+    $krozni_lok = 0x123abc;
+    $moja_spremenljivka = 0xffc00;
+    $stevilo = 030115;
+    $stevec = 5;
+
+    $kvadratna_enacba = 12.369;
+    $_12_hrusk = 12.32625;
+    $vaja_20 = 20.000;
+    $a = 0.8; 
+    $b = 0.36; 
+    $c = 1.32; 
+    $d = 0.99; 
+    $e = -0.85;
+    $alfa = 60.0;
+    $euro = 1265.00;
+    $novo = 365.00;
+    $konstanta = 6.125e23;
+
+    $datoteka = 'c:\mapa\besedilo.txt';
+    $naslov = "http://www.ime.si";
+    $stavek = "gori na gori gori";
+    $prazenStavek = "";
+    $navodilo = "odpri datoteko \"index.html\" in jo daj v \\splet\\nivo\\";
+
+    $moski = true;
+    $zaposlen = false;
+    $pregledana_vreca = true;
 ?>
